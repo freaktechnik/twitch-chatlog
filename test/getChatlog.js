ece4ae3a48a5d8c35991a98666bfadd22bfd3ef1
Martin Giger <martin@humanoids.be>
2016-07-23 17:44:49 +0200

    full coverage

--- test/getChatlog.js
+++ test/getChatlog.js
@@ -1,123 +1,159 @@
 import test from 'ava';
-import mock from 'mock-json-api';
 import express from 'express';
 import vod from './api/v79240813.json';
 import rechatMessage from './api/rechat-message.json';
 import { getChatlog } from '../lib';
 
-var s;
-test.before(() => {
+const createServer = (error) => {
     const server = express();
 
-    const mockapi = mock({
-        jsonStore: __dirname + '/data.json',
-        mockRoutes: [
-            {
-                name: "VOD info",
-                mockRoute: '/kraken/videos/v79240813',
-                testScope: "success",
-                jsonTemplate: [
-                    function() {
-                        return JSON.stringify(vod);
-                    }
-                ]
-            },
-            {
-                name: "Chat log fragment",
-                mockRoute: '/rechat-message',
-                testScope: "success",
-                jsonTemplate: [
-                    function() {
-                        return JSON.stringify(rechatMessage);
-                    }
-                ]
-            }
-        ]
-    });
+    error = error || 0;
 
-    server.use(mockapi.registerRoutes);
-    s = server.listen(3001);
-    process.env.TEST = true;
-});
+    if(error != 1) {
+        server.get('/kraken/videos/v79240813', (req, res) => {
+            res.json(vod);
+        });
+    }
+    if(error != 2) {
+        server.get('/rechat-messages', (req, res) => {
+            res.json(rechatMessage);
+        });
+    }
+
+    return new Promise((resolve) => {
+        const s = server.listen(0, () => resolve(s));
+    });
+};
+
+const closeServer = (server) => {
+    return new Promise((resolve) => server.close(resolve));
+};
 
 test("Test getting message", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
         start: 0,
-        length: 30
+        length: 30,
+        testServer: server.address()
     });
 
     t.is(result.length, 1);
     t.deepEqual(result, rechatMessage.data);
+
+    await closeServer(server);
 });
 
 test("Getting multiple fragments using length", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
         start: 0,
-        length: 60
+        length: 60,
+        testServer: server.address()
     });
 
     t.is(result.length, 2);
+
+    await closeServer(server);
 });
 
 test("Getting a partial fragment returns the whole fragment", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
         start: 0,
-        length: 45
+        length: 45,
+        testServer: server.address()
     });
 
     t.is(result.length, 2);
+
+    await closeServer(server);
 });
 
 test("Getting a partial log based on relative end", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
-        end: "00:03:00"
+        end: "00:03:00",
+        testServer: server.address()
     });
 
     t.is(result.length, 6);
+
+    await closeServer(server);
 });
 
 test("Getting a partial log based on relative start and end", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
         start: "00:01:00",
-        end: "00:03:00"
+        end: "00:03:00",
+        testServer: server.address()
     });
 
     t.is(result.length, 4);
+
+    await closeServer(server);
 });
 
 test("Getting a partial log based on absolute start and relative end", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
         start: new Date(Date.parse(vod.recorded_at)+60000).toString(),
-        end: "00:03:20"
+        end: "00:03:20",
+        testServer: server.address()
     });
 
     t.is(result.length, 5);
+
+    await closeServer(server);
 });
 
 test("Getting a partial log based on absolute end", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
         vodId: vod._id,
-        end: new Date(Date.parse(vod.recorded_at)+60000).toString()
+        end: new Date(Date.parse(vod.recorded_at)+60000).toString(),
+        testServer: server.address()
     });
 
     t.is(result.length, 2);
+
+    await closeServer(server);
 });
 
 test("Getting all fragments to a vod", async function(t) {
+    const server = await createServer();
     const result = await getChatlog({
-        vodId: vod._id
+        vodId: vod._id,
+        testServer: server.address()
     });
 
     t.is(result.length, Math.ceil(vod.length/30));
+
+    await closeServer(server);
 });
 
-test.after(() => {
-    s.close();
-    process.env.TEST = false
+test("Error fetching VOD info", async (t) => {
+    const server = await createServer(1);
+    await t.throws(getChatlog({
+        vodId: vod._id,
+        testServer: server.address()
+    }));
+
+    await closeServer(server);
+});
+
+test("Error fetching log frament", async (t) => {
+    const server = await createServer(2);
+    await t.throws(getChatlog({
+        vodId: vod._id,
+        testServer: server.address()
+    }));
+
+    await closeServer(server);
 });
